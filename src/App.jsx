6998a6e8756bc4b145e5c90eebdc9a9f378aa826
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-07-26 18:24:48 +0100

    Initialized and Set-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { LandingPage } from './component/LandingPage'
 
 function App() {
   return (
-    <div className='bg-amber-500'>App</div>
+   <>
+    <LandingPage />
+   </>
   )
 }
 
